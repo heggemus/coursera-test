17f977df36ac9c1a5412bfe5ca057821a7d0ff4d
bhr_mesle500 <karim.meslem@kpn.com>
2020-11-08 08:41:29 +0100

    submitting assignment4

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,6 +3,7 @@ My Module 4 solution
 
 This is the expected output:
 Hello Karim
+Good Bye Donald Trump
 Good Bye John
 Good Bye Jen
 Good Bye Jason
@@ -16,13 +17,13 @@ Good Bye Jim
 
 (function () {
 
-    var names = ["Karim", "Donald Trump", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0; i < names.length; i++) {
-      var firstletter = names[i].charAt(0).toLowerCase();
-      if (firstletter === 'j' || firstletter === 'd') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+  var names = ["Karim", "Donald Trump", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+    var firstletter = names[i].charAt(0).toLowerCase();
+    if (firstletter === 'j' || firstletter === 'd') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
     }
+  }
 })();
